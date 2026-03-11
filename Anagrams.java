4870fc053f0265db603e3a144801de7a275fb627
AyabongaGcina <4446494@myuwc.ac.za>
2026-03-11 12:22:15 +0000

    Added the java packages

---
+++ Anagrams.java
@@ -0,0 +1,12 @@
+//Ayabonga Gcina
+//4446494
+//Practical 5
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Hashmap;
+import java.util.List;
+import java.util.Map;
